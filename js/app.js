58e7ff2e66679b77c62e791a28e997ec909b3f80
renanns <atomsfrontenf@gmail.com>
2024-05-22 15:53:25 -0300

    fix: changed api to search data and refactor functions to data processing

--- js/app.js
+++ js/app.js
@@ -6,25 +6,26 @@ const climate = document.getElementById('imgClimate');
 const place = document.getElementById('place');
 const degress = document.getElementById('degress');
 const wind = document.getElementById('wind');
+const desc = document.getElementById('desc');
 
-function loadData(data) {
-    console.log(data)
+const loadData = (data) => {        
     content.style.display = 'flex';
     climate.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`; 
-    degress.innerHTML = `Temperatura: ${Math.floor(data.main.temp)} C`;
+    degress.innerHTML = `Temperatura: ${Math.floor(data.main.temp)} Fº`;
     place.innerHTML = `${data.name}, ${data.sys.country}`;
     wind.innerHTML = `Vento: ${data.wind.speed} km/h`;
+    desc.innerHTML = `Sky: ${data.weather[0].description}`;
 };
 
-async function getDataApi() {
-    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=44.34&lon=10.99&cnt=7&appid=f56798495a16c23974ec0f0ef1a35c00}`;
+const getDataApi = async (region) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${region},uk&APPID=f56798495a16c23974ec0f0ef1a35c00`;
 
     try {
         await fetch(url)
                 .then((res) => res.json())
                 .then((data) => {
             
-                    if (data?.cod && data.cod === "404") {
+                    if (!data || data.cod === "404") {
                         return alert(`Não Encontrado!!!`);
                     };
 
@@ -36,11 +37,10 @@ async function getDataApi() {
     };
 };  
 
-share.addEventListener('click', (target) => {
-    console.log(target)
+share.addEventListener('click', () => {
     if (!region.value) {
         return console.error("Input not be empty");
     };
 
-    getDataApi();
+    getDataApi(region.value);
 });
